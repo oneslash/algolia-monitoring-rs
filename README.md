5c4f6c415cd48ec7b11283489dffc8c90033f447
Sardorbek Pulatov <sardo@hey.com>
2024-07-28 19:51:58 +0200

    update deps and improve readme

--- README.md
+++ README.md
@@ -31,6 +31,5 @@ async fn main() {
     let monitoring = AlgoliaMonitoring::new("api_key".to_owned(), "app_id".to_owned());
     let result = monitoring.get_status(None).await;
     println!("{:?}", result);
-    println!("Hello, world!");
 }
 ```
--- examples/basic_example.rs
+++ examples/basic_example.rs
@@ -5,5 +5,4 @@ async fn main() {
     let monitoring = AlgoliaMonitoring::new(Some("api_key".to_owned()), Some("app_id".to_owned()));
     let result = monitoring.get_status(None).await;
     println!("{:?}", result);
-    println!("Hello, world!");
 }
